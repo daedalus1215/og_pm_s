864856d1446fea8f8eb312321585234a4eb49067
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2022-06-04 22:59:20 -0400

    create user endpoint

--- user/.venv/routes.py
+++ user/.venv/routes.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from models import db, User
+from werkzeug.security import generate_password_hash, check_password_hash
 user_blueprint = Blueprint('user_api_routes',
                            __name__,
                            url_prefix='/api/user')
@@ -13,6 +14,25 @@ def get_all_users():
         'result': result
     }
     return jsonify(response);
+
+
+@user_blueprint.route('/create', methods=['POST'])
+def create_user():
+    try:
+        user = User()
+        user.username = request.form["username"]
+        user.password = generate_password_hash(request.form['password'], method='sha256')
+        user.is_admin = True
+
+        db.session.add(user)
+        db.session.commit()
+        response = {'message': 'User Created', 'result':user.serialize()}
+    except Exception as e:
+        print(str(e))
+        response = {'message': 'Error in creating response'}
+    return jsonify(response)
+
+
 
 
 def index():
