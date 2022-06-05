5fe0338ae7f71cde0c9e5027e9078bc16f4e46fd
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2022-06-04 22:47:05 -0400

    Bring in and use flask_migrate

--- user/.venv/app.py
+++ user/.venv/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_migrate import Migrate
 import models
 from routes import user_blueprint
 
@@ -9,5 +10,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./database/user.db'
 models.init_app(app)
 app.register_blueprint(user_blueprint)
 
+migrate = Migrate(app, models.db)
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
