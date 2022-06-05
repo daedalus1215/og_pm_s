124487783efcdf708f8fafd57a3019cc74737f00
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2022-06-04 22:31:34 -0400

    Fix the errors

--- user/.venv/app.py
+++ user/.venv/app.py
@@ -4,8 +4,10 @@ from routes import user_blueprint
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'vZK7siKrV8BL46KsyAIPoQ'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./database/user.db'
 models.init_app(app)
 app.register_blueprint(user_blueprint)
 
-app.run()
+if __name__ == "__main__":
+    app.run()
