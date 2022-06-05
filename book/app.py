55821318e2f7960be301a894c2c815ba1e474146
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2022-06-05 15:18:39 -0400

    wire in blueprint routes

--- book/app.py
+++ book/app.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from routes import book_blueprint
 
 app = Flask(__name__)
 app.config['SECRET_KEY']='cwW2skhDXdNDZP5DzZpWTA'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./database/book.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+app.register_blueprint(book_blueprint)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5002)
